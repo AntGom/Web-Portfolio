f54d6400d75c35c044ace19691c36808e987ea78
Antonio Gómez <antgomdgz@gmail.com>
2025-06-29 18:18:39 +0200

    nuevo cv

--- web-portfolio/src/components/AboutComponents/AboutButtons.jsx
+++ web-portfolio/src/components/AboutComponents/AboutButtons.jsx
@@ -2,7 +2,7 @@ import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const AboutButtons = () => (
   <div className="flex flex-col md:flex-row items-center mt-4 gap-2">
-    <a href="/Web-Portfolio/CV_Antonio_gomez_dmgz.pdf" download>
+    <a href="/Web-Portfolio/CV_agomezdo.pdf" download>
       <button className="bg-[#ffa53b] text-gray-50 px-8 py-2 text-md rounded-full hover:bg-[#0f406a] transition-colors flex items-center gap-x-2 border-2 border-[#ffa53b] hover:border-[#0f406a]">
         Descargar CV
       </button>
